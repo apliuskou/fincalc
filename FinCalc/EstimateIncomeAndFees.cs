4c78281b4ffbbee9ae1f651cab23c18111205eed
fincalc <777alexbel777@gmail.com>
2017-02-08 16:36:44 +0300

    Initial commit

---
+++ FinCalc/EstimateIncomeAndFees.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinCalc
+{
+    //Почему в functions and tests мы сделали аналогичный класс static'ом? 
+    public static class EstimateIncomeAndFees
+    {
+        //Пока не используем сложные проценты
+        //??если класс static, то он заставляет все переменные внутри себя делать const?
+        //??и оно заставляет сразу переменные объявлять и приваивать им значения
+
+        
+        const double rateOfReturn = 0.15; //Пока объявим фиксированный уровень доходности в 15% 
+        const double minimalRateOfReturn = 0.10; //и сочтём успехом доходность более 10% годовых
+
+        //??Попытался присвоить ранее объявленным переменным значения. Облом! см.ниже.
+        //??Пришлось 4 строки ниже закоментить, ибо красным подчёркивает
+        /*successFee = 0.2; 
+        fixedManagementFee = 500; 
+        persentManagementFee = 0.02; 
+        successFee = 0.2; */
+
+
+        public static decimal CalculateManagerialFees(double amountInvested, double investmentPeriodInYears)
+        {
+            //Вознаграждение управляющего инвестициями составит 2% от инвестированной суммы, но не менее USD 999.
+            double fixedManagementFee = 999;
+
+            double persentManagementFee = 0.02;
+            //Success Fee (плата за успех) составит 20% от доходности свыше 10%
+            double successFee = 0.2;
+            double levelOfSuccess = 0.1;
+
+            //Считаем стандартную стоимость услуг инвесткомпании
+            persentManagementFee = persentManagementFee * (amountInvested *1000* investmentPeriodInYears);
+
+
+            if (persentManagementFee < fixedManagementFee)//если 2% от управляемой суммы составят менее 999 долларов, то...
+            { persentManagementFee = fixedManagementFee; }//вознаграждение инвестиционной компании составит 999 долларов
+            else { persentManagementFee = persentManagementFee; }//иначе мы берём значение persentManagementFe в предыдущем выражении
+
+            //Считаем Success Fee
+            successFee = successFee * (rateOfReturn - levelOfSuccess) * amountInvested*1000;
+
+
+            double managementFee = persentManagementFee +successFee;
+
+            decimal _managementFee = Convert.ToDecimal(managementFee);
+
+            return _managementFee;
+        }
+
+
+        public static decimal CalculateIncome(double amountInvested, double investmentPeriodInYears)
+        {//Варварство, но домножил на 1000 вручную, иначе неверный income выходит. И НЕ ПОМОГЛО!
+        double income = amountInvested*1000*investmentPeriodInYears*rateOfReturn;
+        decimal _income = Convert.ToDecimal(income);
+
+        return _income;
+
+        }
+
+        }
+    }
---
+++ FinCalc/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This app is a special software to make investment decisions 
+            // На сколько корректно все переменные ставить double? Я сделал так, чтоб не заморачиваться пока на конвертацию одних форматов в другие
+            // 
+
+            double amountInvested;
+            
+            double investmentPeriodInYears;
+            string userInput = "";
+            decimal finalIncome;
+            decimal allManagerialFees;
+
+
+            // итак, калькулятор работает пока пользователь не ввел 00. Хм,а если подобрать какой-то другой вариант
+            while (userInput != "00")
+            {
+                // Информируем пользователя, что он должен ввести сумму, которую готов инвестировать
+                Console.WriteLine("Welcome to the FinCalc investment. Enter the amount you are ready to invest (thousands of dollar): ");
+                // затем читаем эту сумму в строковую переменную
+                userInput = Console.ReadLine();
+
+                // а теперь конвертируем строку в дробь
+                if (double.TryParse(userInput, out amountInvested) && amountInvested >= 100) // читается "получилось сделать цифру и она больше или равна 100"...
+                {
+                    //amountInvested = amountInvested * 1000; Не работает! На finalIncome это действие не влияет. Потому закомментил.
+                    amountInvested = Convert.ToDouble(userInput); // строго говоря, TryParse уже произвел результат, но так наверное понятнее? :)
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, you can invest not less then USD 100K! Starting over...\n\n");
+                    continue;
+                }
+
+                // Спрашиваем пользователя,на какой период он готов разместить деньги
+                Console.WriteLine("Enter the period your are ready to invest (years): ");
+                // затем читаем ширину в строковую переменную
+                userInput = Console.ReadLine();
+                // а теперь конвертируем строку в дробь
+
+                if (double.TryParse(userInput, out investmentPeriodInYears) && investmentPeriodInYears >= 1)
+                {
+                    investmentPeriodInYears = Convert.ToDouble(userInput);
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect input, investment period must be 1 year or more! Starting over...\n\n");
+                    continue;
+                }
+
+                // Теперь мы вызываем функцию из класса EstimateIncomeAndFees и...
+                // сохраняем результат в нашу переменную:
+                finalIncome = EstimateIncomeAndFees.CalculateIncome(amountInvested, investmentPeriodInYears);
+
+                // если вдруг мы таки получили -1, что сейчас невозможно, но вдруг?
+                if (finalIncome <= 0)
+                {
+                    Console.WriteLine("Something went terribly wrong, try to start over!");
+                    continue;
+                }
+
+                allManagerialFees = EstimateIncomeAndFees.CalculateManagerialFees(amountInvested, investmentPeriodInYears);
+                
+
+                // у нас есть цена и мы выводим ее на экран
+                Console.WriteLine("You can earn " + finalIncome + "$. Managerial fees are " + allManagerialFees + "$.");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
